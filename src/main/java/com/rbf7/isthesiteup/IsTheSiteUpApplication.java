54395f7cfd5e05a01bffc41603b118670636d79c
Renato Ferrante <reg@rbf7.com>
2022-01-06 18:26:30 -0300

    First Commit

---
+++ src/main/java/com/rbf7/isthesiteup/IsTheSiteUpApplication.java
@@ -0,0 +1,14 @@
+package com.rbf7.isthesiteup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsTheSiteUpApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test123");
+		SpringApplication.run(IsTheSiteUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbf7/isthesiteup/IsTheSiteUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbf7.isthesiteup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsTheSiteUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
